d2bdca62b311476e7a65b49720050bf5441892b1
ilyakuzovkin <ilya.kuzovkin@gmail.com>
2017-05-29 11:01:07 +0300

    Collect permutation classification data for LFP

--- Classification/rf_on_lfp.py
+++ Classification/rf_on_lfp.py
@@ -10,13 +10,20 @@ from sklearn.metrics import confusion_matrix, f1_score, accuracy_score
 parser = argparse.ArgumentParser(description='Creates various datasets out of the processed intracranial data')
 parser.add_argument('-f', '--featureset', dest='featureset', type=str, required=True, help='Directory with brain features (Processed/?)')
 parser.add_argument('-s', '--sid', dest='sid', type=int, required=True, help='Subject ID')
+parser.add_argument('-p', '--permutation', dest='permutation', type=bool, required=False, default=False, help='Whether to shuffle data for a permutation test')
 args = parser.parse_args()
 featureset = str(args.featureset)
 sid = int(args.sid)
+permutation = bool(args.permutation)
 
 #: Paths
 DATADIR = '../../Data/Intracranial/Processed'
-OUTDIR = '../../Outcome/Single Probe Classification/LFP/Predictions'
+if permutation:
+    OUTDIR = '../../Outcome/Single Probe Classification/LFP/Permutation'
+else:
+    OUTDIR = '../../Outcome/Single Probe Classification/LFP/Predictions'
+
+print OUTDIR
 
 #: Data parameters
 nstim = 401
@@ -53,8 +60,10 @@ if nprobes > 0:
     for pid in range(data.shape[1]):
         clf = RandomForestClassifier(n_estimators=1000, n_jobs=4)
         
-        predicted = cross_val_predict(clf, signal[:, pid, :], stimgroups, cv=skf)
-        #predicted = cross_val_predict(clf, signal[:, pid, :], np.random.permutation(stimgroups), cv=skf)
+        if permutation:
+            predicted = cross_val_predict(clf, signal[:, pid, :], np.random.permutation(stimgroups), cv=skf)
+        else:
+            predicted = cross_val_predict(clf, signal[:, pid, :], stimgroups, cv=skf)
         
         cms[pid] = {'true':list(stimgroups), 'pred':list(predicted)}
         print pid, '\t', np.max(f1_score(stimgroups, predicted, average=None))
