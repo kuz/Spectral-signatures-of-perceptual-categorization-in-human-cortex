be1827095828b370a331b5ad1d6ccc64f6de6015
ilyakuzovkin <ilya.kuzovkin@gmail.com>
2017-05-30 09:51:02 +0300

    Train classifier on FT features

--- Classification/README.md
+++ Classification/README.md
@@ -1,3 +1,9 @@
 ## Order of execution
 
+#### RF on LFP features
+* `rf_on_lfp.sh` to compute predictions for every probe
+To continue move to the `Analysis` directory
+
+#### RF on FT features
+
 1. Run `rf.py -f mean_5hz9_0ms200_LFP_8c_artif_bipolar_BA_responsive`
--- Classification/rf_on_ft.sh
+++ Classification/rf_on_ft.sh
@@ -3,7 +3,8 @@
 INDATA='ft_4hz150_LFP_8c_artif_bipolar_BA_responsive'
 
 # max 97
-for sid in $(seq 0 1 9)
+#for sid in $(seq 10 1 97)
+for sid in 82 83 84 98
 do
     srun --partition=long,phi,main -c 16 --mem=20000 -t 48:00:00 python rf_on_ft.py -f $INDATA -s $sid &
     sleep 2
