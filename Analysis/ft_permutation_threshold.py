b6e695dd28da6b0f25a609cd8048caafbf672df3
ilyakuzovkin <ilya.kuzovkin@gmail.com>
2017-06-05 12:37:13 +0300

    Threshold estimation for FT-based classifier

---
+++ Analysis/ft_permutation_threshold.py
@@ -0,0 +1,31 @@
+import os
+import numpy as np
+from sklearn.metrics import confusion_matrix, f1_score
+import matplotlib
+matplotlib.use('Agg')
+from matplotlib import pylab as plt
+import matplotlib.cm as cm
+import scipy.io as sio
+from sklearn.ensemble import RandomForestClassifier
+
+INDIR  = '../../Outcome/Single Probe Classification/FT/Permutation'
+OUTDIR = '../../Outcome'
+n_classes = 8
+
+filelist = os.listdir(INDIR)
+n_subjects = len(filelist)
+
+# build matrix CLASSES x PROBES x SUBJECTS with F1 scores
+print 'Aggregating F1 scores ...'
+f1_scores = np.zeros((n_classes, 200, n_subjects))
+for sid, filename in enumerate(filelist):
+    data = np.load('%s/%s' % (INDIR, filename))
+    for pid in data[()].keys():
+        f1_scores[:, pid, sid] = f1_score(data[()][pid]['true'], data[()][pid]['pred'], average=None)
+
+plt.figure(dpi=100);
+plt.hist(np.ravel(f1_scores[f1_scores > 0.0]), 50, facecolor='green', alpha=0.75);
+plt.savefig('%s/Figures/%s' % (OUTDIR, 'FT_classification_f1_distribution.png'), bbox_inches='tight');
+plt.clf();
+
+print 'Out of 11293 permutations the highest F1 score was %.4f' % np.max(f1_scores)
--- Classification/rf_on_ft.py
+++ Classification/rf_on_ft.py
@@ -10,13 +10,18 @@ from sklearn.metrics import confusion_matrix, f1_score
 parser = argparse.ArgumentParser(description='Train separate RF on each probe of a given subject and store predictions')
 parser.add_argument('-f', '--featureset', dest='featureset', type=str, required=True, help='Directory with brain features (Processed/?)')
 parser.add_argument('-s', '--sid', dest='sid', type=int, required=True, help='Subject ID')
+parser.add_argument('-p', '--permutation', dest='permutation', type=bool, required=False, default=False, help='Whether to shuffle data for a permutation test')
 args = parser.parse_args()
 featureset = str(args.featureset)
 sid = int(args.sid)
+permutation = bool(args.permutation)
 
 #: Paths
 DATADIR = '../../Data/Intracranial/Processed'
-OUTDIR = '../../Outcome/Single Probe Classification/FT/Predictions'
+if permutation:
+    OUTDIR = '../../Outcome/Single Probe Classification/FT/Permutation'
+else:
+    OUTDIR = '../../Outcome/Single Probe Classification/FT/Predictions'
 
 #: Data parameters
 nstim = 401
@@ -35,6 +40,7 @@ cms = {}
 sfile = subjects[sid]
 s = sio.loadmat('%s/%s/%s' % (DATADIR, 'LFP_8c_artif_bipolar_BA_responsive', sfile))
 sname = s['s']['name'][0][0][0]
+print 'Processing', sname
 areas = np.ravel(s['s']['probes'][0][0][0][0][3])
 n_probes = len(areas)
 stimgroups = np.ravel(s['s']['stimgroups'][0][0])
@@ -58,8 +64,11 @@ if n_probes > 0:
         signal = signal.reshape(signal.shape[0], signal.shape[1] * signal.shape[2])
 
         # train a classifier
-        clf = RandomForestClassifier(n_estimators=3000, n_jobs=16)
-        predicted = cross_val_predict(clf, signal, stimgroups, cv=skf)
+        clf = RandomForestClassifier(n_estimators=3000, n_jobs=10)
+        if permutation:
+            predicted = cross_val_predict(clf, signal, np.random.permutation(stimgroups), cv=skf)
+        else:
+            predicted = cross_val_predict(clf, signal, stimgroups, cv=skf)
         clf = None
         
         # store and show results
