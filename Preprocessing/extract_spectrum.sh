20c4deccd67e98a8bddcfc935f18b10b52f8e220
ilyakuzovkin <ilya.kuzovkin@gmail.com>
2017-06-05 12:36:01 +0300

    (minor)

--- Preprocessing/extract_spectrum.sh
+++ Preprocessing/extract_spectrum.sh
@@ -2,8 +2,8 @@
 
 INDATA='LFP_8c_artif_bipolar_BA_responsive'
 
-for sid in $(seq 1 1 10)
+for sid in $(seq 1 1 98)
 do
-    srun --partition=long,phi,main -c 2 --mem=2000 -t 36:00:00 /storage/software/MATLAB_R2013b/bin/matlab -nojvm -nodisplay -nosplash -r "indata='$INDATA'; sid=$sid; extract_spectrum; exit" &
+    srun --partition=long,phi,main -c 2 --mem=2000 -t 48:00:00 /storage/software/MATLAB_R2013b/bin/matlab -nojvm -nodisplay -nosplash -r "indata='$INDATA'; sid=$sid; extract_spectrum; exit" &
     sleep 2
 done
