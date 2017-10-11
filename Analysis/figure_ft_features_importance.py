6f03b7be075ae95e8ee8d9aab306dadf695ba50b
ilyakuzovkin <ilya.kuzovkin@gmail.com>
2017-10-11 22:32:40 +0300

    work in progress on figure with top 4 clusters on it

--- Analysis/figure_ft_features_importance.py
+++ Analysis/figure_ft_features_importance.py
@@ -9,6 +9,7 @@ from surfer import Brain
 
 # parameters
 INDIR = '../../Outcome/Single Probe Classification/FT/Importances'
+CLUSTDIR = '../../Outcome/Clustering'
 DATADIR = '../../Data/Intracranial/Processed'
 OUTDIR = '../../Outcome/Figures'
 
@@ -17,19 +18,39 @@ subject_id = "fsaverage"
 subjects_dir = os.environ["SUBJECTS_DIR"]
 
 # lists
-categories = ['house', 'visage', 'animal', 'scene', 'tool', 'pseudoword', 'characters', 'scrambled']
+#categories = ['house', 'visage', 'animal', 'scene', 'tool', 'pseudoword', 'characters', 'scrambled']
+categories = ['house']
 subjlist = sorted(os.listdir('../../Outcome/Single Probe Classification/FT/Predictions'))
 n_subjects = len(subjlist)
 scores_spc = np.load('%s/../scores_sid_pid_cat.npy' % INDIR).item()
 
-# 
-importance_allctg = np.zeros((0, 146, 48))
+def safemkdir(path):
+    try:
+        os.mkdir(path)
+    except:
+        pass
 
-def triptych(m, foci, title, filenames):
+# plot definition
+def cluster_mean(activity, title, cluster_color):
+    fig = plt.figure(figsize=(8, 6), dpi=300);
+    plt.imshow(d, interpolation='none', origin='lower', cmap=cm.bwr, aspect='auto', vmin=-3.0, vmax=3.0);
+    plt.colorbar();
+    plt.axvline(x=16, ymin=0.0, ymax = 1.0, linewidth=1.0, color='r', ls='--');
+    plt.xticks(np.arange(0, 48), np.asarray((np.arange(0, 769, 16) - 256) / 512.0 * 1000, dtype='int'), size=5, rotation=90);
+    plt.yticks(np.arange(0, 146, 5), np.arange(4, 150, 5), size=5);
+    plt.ylabel('Frequency (Hz)', size=10);
+    plt.xlabel('Time (30 ms bin)', size=10);
+    plt.title(title, size=11);
+    plt.savefig(fname, bbox_inches='tight');
+    plt.clf();
+    plt.close(fig);
 
-    fig = plt.figure(figsize=(24, 6), dpi=300);
-    plt.subplot(1, 3, 1);
-    plt.imshow(m, interpolation='none', origin='lower', cmap=cm.Blues, aspect='auto');
+
+def quadriptych(importances, foci, foci_colors, title, filenames):
+
+    fig = plt.figure(figsize=(32, 6), dpi=300);
+    plt.subplot(1, 5, 1);
+    plt.imshow(importances, interpolation='none', origin='lower', cmap=cm.Blues, extent=(0, 100, 0, 100), aspect='equal');
     plt.colorbar();
     plt.axvline(x=16, ymin=0.0, ymax = 1.0, linewidth=1.0, color='r', ls='--')
     plt.xticks(np.arange(0, 48), np.asarray((np.arange(0, 769, 16) - 256) / 512.0 * 1000, dtype='int'), size=5, rotation=90);
@@ -38,10 +59,20 @@ def triptych(m, foci, title, filenames):
     plt.xlabel('Time (30 ms bin)', size=10);
     plt.title(title, size=11);
 
+    plt.subplot(2, 5, 2);
+    plt.imshow(importances, interpolation='none', origin='lower', cmap=cm.Blues, aspect=0.3);
+    plt.subplot(2, 5, 3);
+    plt.imshow(importances, interpolation='none', origin='lower', cmap=cm.Blues, aspect=0.3);
+    plt.subplot(2, 5, 7);
+    plt.imshow(importances, interpolation='none', origin='lower', cmap=cm.Blues, aspect=0.3);
+    plt.subplot(2, 5, 8);
+    plt.imshow(importances, interpolation='none', origin='lower', cmap=cm.Blues, aspect=0.3);
+
     # 3D mesh
-    plt.subplot(1, 3, 2);
+    plt.subplot(1, 5, 4);
     brain = Brain(subject_id, "both", "pial", cortex='ivory', alpha=0.4, background='white');
-    brain.add_foci(foci, hemi='rh', scale_factor=0.5, color='blue');
+    for color in np.unique(foci_colors):
+        brain.add_foci(foci[foci_colors==color, :], hemi='rh', scale_factor=0.5, color=color);
     brain.show_view('m');
     pic = brain.screenshot()
     plt.imshow(pic);
@@ -49,9 +80,10 @@ def triptych(m, foci, title, filenames):
     plt.ylim(800, 0);
 
     # 3D mesh
-    plt.subplot(1, 3, 3);
+    plt.subplot(1, 5, 5);
     brain = Brain(subject_id, "both", "pial", cortex='ivory', alpha=0.4, background='white');
-    brain.add_foci(foci, hemi='rh', scale_factor=0.5, color='blue');
+    for color in np.unique(foci_colors):
+        brain.add_foci(foci[foci_colors==color, :], hemi='rh', scale_factor=0.5, color=color);
     brain.show_view(view=dict(azimuth=0.0, elevation=0), roll=90);
     pic = brain.screenshot()
     plt.imshow(pic);
@@ -64,24 +96,40 @@ def triptych(m, foci, title, filenames):
     plt.close(fig);
 
 
-
 # separate plot for each category
 for cid, category in enumerate(categories):
 
     print 'Drawing "%s" ...' % category
-
     subdir = 'FT_importances_%d_%s' % (cid, category)
-    try:
-        os.mkdir('%s/%s' % (OUTDIR, subdir))
-    except:
-        pass
+    safemkdir('%s/%s' % (OUTDIR, subdir))
 
     importance = np.load('%s/%s' % (INDIR, 'FT_feature_importances_ctg%d.npy' % cid))
     successful_probes = np.load('%s/%s' % (INDIR, 'FT_successful_probes_ctg%d.npy' % cid))
     successful_mnis = np.load('%s/%s' % (INDIR, 'FT_successful_mnis_ctg%d.npy' % cid))
     successful_areas = np.load('%s/%s' % (INDIR, 'FT_successful_areas_ctg%d.npy' % cid))
-    
-    importance_allctg = np.concatenate((importance_allctg, importance))
+    cluster_labels = np.load('%s/%d-%s/successful_probes_to_cluster_labels.npy' % (CLUSTDIR, cid, categories[cid]))
+    important_activity_patterns = np.load('%s/%d-%s/important_activity_patterns.npy' % (CLUSTDIR, cid, categories[cid]))
+
+    # four most important clusters' sample means
+    # here we manually specify for every category which cluster ID's are the most important ones
+    important_clusters_per_cid = {
+        0: [1, 4, 5, 6],
+        1: [],
+        2: [],
+        3: [],
+        4: [],
+        5: [],
+        6: [],
+        7: []
+    }
+    important_clusters = important_clusters_per_cid[cid]
+
+    # Category mean, 4 clusters, locations
+    #colors = np.array(['blue' for x in range(successful_mnis.shape[0])])
+    colors = np.array(['blue' for x in range(23)] + ['green' for x in range(23)])
+    quadriptych(np.mean(importance, 0), successful_mnis, colors,
+                'Importance of spectrotemporal features for "%s"' % categories[cid],
+                ['%s/FT_importances_%d_%s_MEAN.png' % (OUTDIR, cid, category)])
 
     # importance in time
     #most_important_moment = np.argmax(np.sum(importance[:, :, :], axis=1), axis=1)
@@ -117,6 +165,7 @@ for cid, category in enumerate(categories):
     """
 
     # Mono and Poly predictive probes
+    """
     monoprobes = []
     polyprobes = []
     for i, (pid, sid) in enumerate(successful_probes):
@@ -135,6 +184,7 @@ for cid, category in enumerate(categories):
              'Importance of spectrotemporal features for "%s"\nmean over %d polypredictive probes' % (categories[cid], len(polyprobes)),
              ['%s/%s/FT_importances_%d_%s_POLY_MEAN.png' % (OUTDIR, subdir, cid, category)])
     print 'BAs: ', successful_areas[polyprobes]
+    """
     
 
     """
@@ -176,11 +226,4 @@ for cid, category in enumerate(categories):
         triptych(np.mean(importance[successful_areas == ba, :, :], 0), successful_mnis[successful_areas == ba],
                  'Importance of spectrotemporal features for "%s"\nmean over %d probes in BA%d' % (categories[cid], np.sum(successful_areas == ba), ba),
                  ['%s/%s/FT_importances_%d_%s_MEAN_BA%d.png' % (OUTDIR, subdir, cid, category, ba)])
-    """
-
-    # Mean over all whole category
-    """
-    triptych(np.mean(importance, 0), successful_mnis,
-             'Importance of spectrotemporal features for "%s"' % categories[cid],
-             ['%s/FT_importances_%d_%s_MEAN.png' % (OUTDIR, cid, category)])
     """
